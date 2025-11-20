97fa947e45ef950ce6704d97457c6e526dd46e1b
lukekrula <Drcek.drtikol@gmail.com>
2025-11-20 21:27:40 +0100

    Added database

---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO items (title, description, done) VALUES ( 'Super polozka', 'Nejlepsi item na svete', FALSE);
+INSERT INTO items (title, description, done) VALUES ('Phone', 'Smartphone with 5G', false);
+INSERT INTO items (title, description, done) VALUES ('Laptop', 'Lightweight ultrabook', false);
+INSERT INTO items (title, description, done) VALUES ('Headphones', 'Noise-cancelling', true);
