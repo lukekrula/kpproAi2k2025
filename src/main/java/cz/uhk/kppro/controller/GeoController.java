17c2f53a57b12fb0c09b33767367a2e41d72b12f
lukekrula <Drcek.drtikol@gmail.com>
2025-12-08 19:30:51 +0100

    more layers

--- src/main/java/cz/uhk/kppro/controller/GeoController.java
+++ src/main/java/cz/uhk/kppro/controller/GeoController.java
@@ -21,4 +21,37 @@ public class GeoController {
             return ResponseEntity.ok(json);
         }
     }
+
+    @GetMapping(value = "/api/arch", produces = "application/json")
+    public ResponseEntity<String> getArch() throws IOException {
+
+        ClassPathResource resource = new ClassPathResource("static/data/architecture.json");
+
+        try (InputStream in = resource.getInputStream()) {
+            String json = new String(in.readAllBytes());
+            return ResponseEntity.ok(json);
+        }
+    }
+
+    @GetMapping(value = "/api/nature", produces = "application/json")
+    public ResponseEntity<String> getNature() throws IOException {
+
+        ClassPathResource resource = new ClassPathResource("static/data/nature.json");
+
+        try (InputStream in = resource.getInputStream()) {
+            String json = new String(in.readAllBytes());
+            return ResponseEntity.ok(json);
+        }
+    }
+
+    @GetMapping(value = "/api/region", produces = "application/json")
+    public ResponseEntity<String> getRegion() throws IOException {
+
+        ClassPathResource resource = new ClassPathResource("static/data/region.json");
+
+        try (InputStream in = resource.getInputStream()) {
+            String json = new String(in.readAllBytes());
+            return ResponseEntity.ok(json);
+        }
+    }
 }
