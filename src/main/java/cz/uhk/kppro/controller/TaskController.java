321e4fcb10edca09c7059085a5957e3cfd24cf2e
lukekrula <Drcek.drtikol@gmail.com>
2025-12-17 15:19:34 +0100

    added task controller, add task, task list

--- src/main/java/cz/uhk/kppro/controller/TaskController.java
+++ src/main/java/cz/uhk/kppro/controller/TaskController.java
@@ -2,6 +2,8 @@ package cz.uhk.kppro.controller;
 
 import cz.uhk.kppro.model.Task;
 import cz.uhk.kppro.service.TaskService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -11,30 +13,29 @@ import java.util.UUID;
 @RequestMapping("/tasks")
 public class TaskController {
 
-    private final TaskService service;
+    private final TaskService taskService;
 
-    public TaskController(TaskService service) {
-        this.service = service;
+    public TaskController(TaskService taskService) {
+        this.taskService = taskService;
     }
 
-    @PostMapping
-    public Task create(@RequestParam String name) {
-        return service.createTask(name);
-    }
 
-    @PostMapping("/{id}/subtask")
-    public void addSubTask(@PathVariable UUID id,
-                           @RequestParam String name) {
-        service.addSubTask(id, name);
-    }
 
-    @PostMapping("/{id}/complete")
-    public void complete(@PathVariable UUID id) {
-        service.completeTask(id);
-    }
 
-    @GetMapping
-    public List<Task> roots() {
-        return service.findAllRootTasks();
+
+        @PostMapping("/create-l")
+        public String createTask(
+                @RequestParam String name,
+                @RequestParam(required = false, name = "subTasks") List<String> subTasks
+        ) {
+            Task task = taskService.createTask(name);
+
+            if (subTasks != null) {
+                subTasks.forEach(sub ->
+                        taskService.addSubTask(task.getId(), sub)
+                );
+            }
+
+            return "redirect:logged";
+        }
     }
-}
