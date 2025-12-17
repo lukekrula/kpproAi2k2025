4576da6d76e7b18f5408c91d31457e22340de990
lukekrula <Drcek.drtikol@gmail.com>
2025-12-17 20:48:02 +0100

    relationship adjustment, dto and mapper

---
+++ src/main/java/cz/uhk/kppro/dto/ActivityDto.java
@@ -0,0 +1,4 @@
+package cz.uhk.kppro.dto;
+
+public class ActivityDto {
+}
--- src/main/java/cz/uhk/kppro/model/Program.java
+++ src/main/java/cz/uhk/kppro/model/Program.java
@@ -2,6 +2,8 @@ package cz.uhk.kppro.model;
 
 import jakarta.persistence.*;
 
+import java.util.List;
+
 @Entity
 @Table(name = "program")
 public class Program {
@@ -12,11 +14,94 @@ public class Program {
 
     private String name;
     private Integer amountMax;
+    private String description;
+
+
+
 
     @ManyToOne
     private Partner partner;
 
+    @ManyToOne
+    @JoinColumn(name = "creator_community_id")
+    private Community creator;
 
+    @ManyToMany
+    @JoinTable(
+            name = "program_shared_communities",
+            joinColumns = @JoinColumn(name = "program_id"),
+            inverseJoinColumns = @JoinColumn(name = "community_id")
+    )
+    private List<Community> sharedWith;
+
+    // members assigned to this program
+    @ManyToMany
+    @JoinTable(
+            name = "program_members",
+            joinColumns = @JoinColumn(name = "program_id"),
+            inverseJoinColumns = @JoinColumn(name = "member_id")
+    )
+    private List<Member> assignedMembers;
+
+    @ManyToOne
+    @JoinColumn(name = "manager_id")
+    private Member manager;
+
+
+    // Tasks belonging to this program
+    @OneToMany(mappedBy = "program", cascade = CascadeType.ALL)
+    private List<Task> tasks;
+
+    @OneToMany(mappedBy = "assignedTo")
+    private List<Task> assignedTasks;
+
+    @ManyToMany(mappedBy = "assignedMembers")
+    private List<Program> assignedPrograms;
+
+    @OneToMany(mappedBy = "manager")
+    private List<Program> managedPrograms;
+
+    public Community getCreator() {
+        return creator;
+    }
+
+    public void setCreator(Community creator) {
+    }
+
+    public List<Community> getSharedWith() {
+        return sharedWith;
+    }
+
+    public void setSharedWith(List<Community> sharedWith) {
+        this.sharedWith = sharedWith;
+    }
+
+    public List<Member> getAssignedMembers() {
+        return assignedMembers;
+    }
+
+    public void setAssignedMembers(List<Member> assignedMembers) {
+        this.assignedMembers = assignedMembers;
+    }
+
+    public Member getManager() {
+        return manager;
+    }
+
+    public void setManager(Member manager) {
+        this.manager = manager;
+    }
+
+    public List<Task> getTasks() {
+        return tasks;
+    }
+    public void setTasks(List<Task> tasks) {
+        this.tasks = tasks;
+    }
+
+    public List<Task> getAssignedTasks() {
+        return assignedTasks;
+    }
     public long getId() {
         return id;
     }
@@ -27,6 +112,10 @@ public class Program {
 
     public String getName() {
         return name;
+    }
+
+    public String getDescription() {
+        return description;
     }
 
     public void setName(String name) {
